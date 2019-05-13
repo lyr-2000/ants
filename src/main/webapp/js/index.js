398c433950dd0a9b4063999c1f457051df6285b8
QZEming <857496538@qq.com>
2019-05-14 00:27:28 +0800

    banner set

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,6 +1,25 @@
 let vm = new Vue({
     el: "#app",
     data: {
-        projectName: '蚂蚁置物'
+        projectName: '蚂蚁置物',
+        wholeClassify: [{
+            src: 'img/shuji.png',
+            name: '书籍'
+        }, {
+            src: '',
+            name: '文具'
+        }, {
+            src: '',
+            name: '美妆'
+        }, {
+            src: '',
+            name: '食品'
+        }, {
+            src: '',
+            name: '电器'
+        }, {
+            src: '',
+            name: '其他'
+        }]
     }
 })
