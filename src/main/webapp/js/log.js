d46d7e614e369084c9e9789949a52cf2a337601e
QZEming <857496538@qq.com>
2019-05-15 02:21:30 +0800

    流星动画实现

--- src/main/webapp/js/log.js
+++ src/main/webapp/js/log.js
@@ -4,19 +4,38 @@ let vm = new Vue({
         productName: "蚂蚁置物",
         school: "",
         sNo: "",
-        password: ""
+        password: "",
+        meteorMoveLength: 100,
+        meteorShow: false, // 表示流星的显示
     },
     methods: {
         login: function() { // 发起登录请求
             axios.post('url', {
-                school: this.data.school,
-                sNo: this.data.sNo,
-                password: this.data.password
+                school: this.school,
+                sNo: this.sNo,
+                password: this.password
             }).then((res) => {
 
             }).catch((err) => {
 
             })
+        },
+        meteorBeforeEnter: function(el) {
+            let clientX = Math.random() * 800 + 100;
+            let clientY = Math.random() * 200;
+            el.style.left = clientX + 'px';
+            el.style.top = clientY + 'px';
+            el.style.opacity = 0;
+        },
+        meteorEnter: function(el, done) {
+            let clientX = parseInt(el.style.left) - this.meteorMoveLength + 'px';
+            let clientY = parseInt(el.style.top) + this.meteorMoveLength + 'px';
+            Velocity(el, { left: clientX, top: clientY, opacity: 1 }, { duration: 1000 });
+        },
+        meteorLeave: function(el, done) {
+            let clientX = parseInt(el.style.left) - this.meteorMoveLength + 'px';
+            let clientY = parseInt(el.style.top) + this.meteorMoveLength + 'px';
+            Velocity(el, { left: clientX, top: clientY, opacity: 0 }, { duration: 1000 });
         }
     },
     components: {
@@ -26,6 +45,16 @@ let vm = new Vue({
                 <slot name="img"></slot>
             </div>`
         }
+    },
+    mounted() {
+        let that = this;
+        setInterval(function() {
+            console.log(1)
+            that.meteorShow = !that.meteorShow;
+            setTimeout(function() {
+                that.meteorShow = false;
+            }, 1000)
+        }, 4000);
     }
 })
 
