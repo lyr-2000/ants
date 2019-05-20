b9a1d5b8e6b119df3dac44cb2816d948e7b075f9
stopyourhand <1783209601@qq.com>
2019-05-20 16:25:16 +0800

    终于搞定这个bug

--- src/main/java/com/ants/controller/UserController.java
+++ src/main/java/com/ants/controller/UserController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author czd
  */
@@ -15,12 +18,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping(value = "/log")
 public class UserController {
 
-    @Autowired
-    private UserService userService;
-
     @RequestMapping(value = "/helloUser")
-    public String helloUser() {
-
-        return "log";
+    @ResponseBody
+    public Map<String, Object> helloUser() {
+        Map<String, Object> ret = new HashMap<>();
+        Map<String, Object> queryMap = new HashMap<>();
+        queryMap.put("content", 1);
+        queryMap.put("offset", 2);
+        queryMap.put("pageSize", 3);
+        ret.put("rows", 4);
+        ret.put("total", 4);
+        return ret;
     }
 }
