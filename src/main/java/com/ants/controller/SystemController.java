26cf782095434babd83605731a59d9e09e2b8261
itdropout <cbcb0204@163.com>
2019-05-27 15:37:17 +0800

    新增token机制，还没解决好。。

--- src/main/java/com/ants/controller/SystemController.java
+++ src/main/java/com/ants/controller/SystemController.java
@@ -127,6 +127,42 @@ public class SystemController {
         }
     }
 
+    @RequestMapping(value = "/testStudent", method = RequestMethod.GET)
+    @ResponseBody
+    public void doToken(HttpServletRequest request,HttpServletResponse response){
+        boolean b = isRepeatSubmit(request);//判断用户是否是重复提交
+                     if(b==true){
+                             System.out.println("请不要重复提交");
+                             return;
+                         }
+                     request.getSession().removeAttribute("token");//移除session中的token
+                     System.out.println("处理用户提交请求！！");
+    }
+
+    /*
+    判断有没有重复提交表单
+     */
+    private boolean isRepeatSubmit(HttpServletRequest request) {
+                     String client_token = request.getParameter("token");
+                     //1、如果用户提交的表单数据中没有token，则用户是重复提交了表单
+                    if(client_token==null){
+                             return true;
+                         }
+                     //取出存储在Session中的token
+                     String server_token = (String) request.getSession().getAttribute("token");
+                     //2、如果当前用户的Session中不存在Token(令牌)，则用户是重复提交了表单
+                     if(server_token==null){
+                             return true;
+                         }
+                     //3、存储在Session中的Token(令牌)与表单提交的Token(令牌)不同，则用户是重复提交了表单
+                     if(!client_token.equals(server_token)){
+                             return true;
+                         }
+
+                     return false;
+    }
+
+
 
     @Autowired
     private StudentService studentService;
