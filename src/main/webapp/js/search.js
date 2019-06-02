7715f649f840f7e28c4eff5172321e20e0d9d4a4
QZEming <857496538@qq.com>
2019-06-03 00:32:27 +0800

    副页面类导航布局

---
+++ src/main/webapp/js/search.js
@@ -0,0 +1,31 @@
+let vm = new Vue({
+    el: '#app',
+    data: {
+        others: false,
+        synthesis: [
+            { parentId: 1, parentName: "书籍", parentPicture: "book.png" },
+            {
+                parentId: 2,
+                parentName: "文具",
+                parentPicture: "stationery.png"
+            },
+            { parentId: 3, parentName: "日用", parentPicture: "daily.png" },
+            { parentId: 4, parentName: "美妆", parentPicture: "cosmetics.png" },
+            { parentId: 5, parentName: "食品", parentPicture: "food.png" },
+            { parentId: 6, parentName: "电器", parentPicture: "electrical.png" },
+            { parentId: 7, parentName: "其他", parentPicture: "others.png" }
+        ],
+        composite: [
+            { subClassId: 0, subClassName: "英语四六级", parentClass: 0 },
+            { subClassId: 0, subClassName: "证从", parentClass: 0 },
+            { subClassId: 0, subClassName: "证从", parentClass: 0 },
+            { subClassId: 0, subClassName: "证从", parentClass: 0 },
+            { subClassId: 0, subClassName: "证从", parentClass: 0 },
+            { subClassId: 0, subClassName: "证从", parentClass: 0 },
+            { subClassId: 0, subClassName: "证从", parentClass: 0 },
+        ],
+    },
+    methods: {
+
+    }
+})
