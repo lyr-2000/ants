736fa574592d1b6ee0aefb3138f2c78e1ae3dfad
晨边啊啊 <cbcb5652@163.com>
2019-09-03 01:53:29 +0800

    '验证校验'

--- src/main/java/com/ants/controller/personal/SlideController.java
+++ src/main/java/com/ants/controller/personal/SlideController.java
@@ -21,6 +21,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -67,36 +68,21 @@ public class SlideController extends HttpServlet {
         }
     }
 
-    @RequestMapping(value = "/checkServlet")
+    @RequestMapping(value = "/checkServlet",method = RequestMethod.POST)
     public void checkCode(HttpServletResponse response, HttpServletRequest request) throws IOException {
         String point = request.getParameter("point");
         Integer location_x = (Integer) request.getSession().getAttribute("location_x");
         HttpSession session = request.getSession();
+        Map map = new HashMap();
         if ((Integer.valueOf(point) < location_x + 4) && (Integer.valueOf(point) > location_x - 4)) {
             //说明验证通过，
-            session.setAttribute("status","验证成功");
-//            session.setAttribute();
-            outData(response, "success");
+            map.put("status",1);
+            map.put("slideCode","dadas");
             System.out.println("验证通过");
         } else {
-            session.setAttribute("status","验证失败");
-            outData(response, "error");
+            map.put("status",0);
+            map.put("slideCode","dasdas");
             System.out.println("验证失败");
-        }
-    }
-
-    private void outData(HttpServletResponse response, Object data) throws IOException {
-        PrintWriter out = response.getWriter();
-        try {
-            out = response.getWriter();
-            response.setContentType("application/json-rpc;charset=UTF-8");
-            out.println(JSON.toJSONString(data));
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if(out != null) {
-                out.close();
-            }
         }
     }
 
